fdb0817c8eae0b9bfedb3fd65477b9527ae120c9
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-07-26 13:26:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import Header from "./Header";
+import Noteslist from "./NotesList";
+
+class App extends Component {
+  state = {
+    notes: [
+      {
+        id: Date.now(),
+        title: "",
+        description: "",
+        doesMatchSearch: true
+      }
+    ],
+    searchText: ""
+  };
+
+  componentDidUpdate() {
+    const savedNotesString = JSON.stringify(this.state.notes);
+    localStorage.setItem("savedNotes", savedNotesString);
+  }
+  componentDidMount() {
+    const savedNotesString = localStorage.getItem("savedNotes");
+    if (savedNotesString) {
+      const savedNotes = JSON.parse(savedNotesString);
+      this.setState({ notes: savedNotes });
+    }
+  }
+
+  addNote = () => {
+    const newNote = {
+      id: Date.now(),
+      title: "",
+      description: "",
+      doesMatchSearch: true
+    };
+    const newNotes = [newNote, ...this.state.notes];
+    this.setState({ notes: newNotes });
+  };
+
+  onType = (editMeId, updatedKey, updatedValue) => {
+    const updatedNotes = this.state.notes.map((note) => {
+      if (note.id !== editMeId) {
+        return note;
+      } else {
+        if (updatedKey === "title") {
+          note.title = updatedValue;
+          return note;
+        } else {
+          note.description = updatedValue;
+          return note;
+        }
+      }
+    });
+    this.setState({ notes: updatedNotes });
+  };
+
+  onSearch = (text) => {
+    const lowerSearchText = text.toLowerCase();
+    const matchingNotes = this.state.notes.map((note) => {
+      if (!lowerSearchText) {
+        note.doesMatchSearch = true;
+        return note;
+      } else {
+        const title = note.title.toLowerCase();
+        const description = note.description.toLowerCase();
+        const titleMatch = title.includes(lowerSearchText);
+        const descriptionMatch = description.includes(lowerSearchText);
+        const hasMatch = titleMatch || descriptionMatch;
+        note.doesMatchSearch = hasMatch;
+        return note;
+      }
+    });
+    this.setState({ notes: matchingNotes, searchText: lowerSearchText });
+  };
+
+  removeNote = (noteId) => {
+    const newNotes = this.state.notes.filter((note) => note.id !== noteId);
+    this.setState({ notes: newNotes });
+  };
+
+  render() {
+    return (
+      <div>
+        <Header
+          onSearch={this.onSearch}
+          addNote={this.addNote}
+          searchText={this.state.searchText}
+        />
+        <Noteslist
+          removeNote={this.removeNote}
+          onType={this.onType}
+          notes={this.state.notes}
+        />
+      </div>
+    );
+  }
+}
+
+export default App;
